9b3622bf5a67826758d83dbc7f1b61ae925186c1
Chris Jungerius <cjungerius@gmail.com>
2020-12-04 08:04:30 +0100

    slight edit on day 4

--- 04/solution.jl
+++ 04/solution.jl
@@ -1,79 +1,79 @@
 function validation(field, value)
-fields = Dict(
-    [
-        (
-            "byr", 
-            function(value)
-                x = tryparse(Int, value)
-                if !isnothing(x)
-                    return 1920 <= x <= 2002
-                else
-                    return false
-                end
-            end
-        ),
-        (
-            "iyr",
-            function(value)
-                x = tryparse(Int, value)
-                if !isnothing(x)
-                    return 2010 <= x <= 2020
-                else
-                    return false
-                end
-            end
-        ),
-        (
-            "eyr",
-            function(value)
-                x = tryparse(Int, value)
-                if !isnothing(x)
-                    return 2020 <= x <= 2030
-                else
-                    return false
-                end
-            end           
-        ),
-        (
-            "hgt",
-            function(value)
-                m = match(r"^(\d+)(cm|in)$",value)
-                if !isnothing(m)
-                    if m.captures[2] == "cm"
-                        return 150 <= parse(Int,m.captures[1]) <= 193
+    fields = Dict(
+        [
+            (
+                "byr", 
+                function(value)
+                    x = tryparse(Int, value)
+                    if !isnothing(x)
+                        return 1920 <= x <= 2002
                     else
-                        return 59 <= parse(Int,m.captures[1]) <= 76
+                        return false
                     end
-                else
-                    return false
                 end
-            end
-        ),
-        (
-            "hcl",
-            function(value)
-                m = match(r"^#[0-9|a-f]{6}$",value)
-                return !isnothing(m)
-            end
-        ),
-        (
-            "ecl",
-            function(value)
-                m = match(r"^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$",value)
-                return !isnothing(m)
-            end
-        ),
-        (
-            "pid",
-            function(value)
-                m = match(r"^\d{9}$",value)
-                return !isnothing(m)
-            end
-        )
-    ]
-)
+            ),
+            (
+                "iyr",
+                function(value)
+                    x = tryparse(Int, value)
+                    if !isnothing(x)
+                        return 2010 <= x <= 2020
+                    else
+                        return false
+                    end
+                end
+            ),
+            (
+                "eyr",
+                function(value)
+                    x = tryparse(Int, value)
+                    if !isnothing(x)
+                        return 2020 <= x <= 2030
+                    else
+                        return false
+                    end
+                end           
+            ),
+            (
+                "hgt",
+                function(value)
+                    m = match(r"^(\d+)(cm|in)$",value)
+                    if !isnothing(m)
+                        if m.captures[2] == "cm"
+                            return 150 <= parse(Int,m.captures[1]) <= 193
+                        else
+                            return 59 <= parse(Int,m.captures[1]) <= 76
+                        end
+                    else
+                        return false
+                    end
+                end
+            ),
+            (
+                "hcl",
+                function(value)
+                    m = match(r"^#[0-9|a-f]{6}$",value)
+                    return !isnothing(m)
+                end
+            ),
+            (
+                "ecl",
+                function(value)
+                    m = match(r"^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$",value)
+                    return !isnothing(m)
+                end
+            ),
+            (
+                "pid",
+                function(value)
+                    m = match(r"^\d{9}$",value)
+                    return !isnothing(m)
+                end
+            )
+        ]
+    )
 
-fields[field](value)
+    fields[field](value)
 end
 
 
