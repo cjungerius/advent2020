da4544722701971d84aae70d57831e27ecd68fcc
Chris Jungerius <cjungerius@gmail.com>
2020-12-05 15:31:10 +0100

    realised seatid is just the full binary number LOL

--- 05/solution1.jl
+++ 05/solution1.jl
@@ -1,9 +1,7 @@
 function seatid(code)
     code = replace(code,r"B|R"=>"1")
     code = replace(code,r"F|L"=>"0")
-    row = parse(Int,code[1:7];base=2)
-    col = parse(Int,code[8:end];base=2)
-    row*8+col
+    parse(Int,code;base=2)
 end
 
 function main(args)
