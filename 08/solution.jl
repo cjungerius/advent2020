c9b53c2975c4fdbf649b6e10282eb8afb2c92fa3
Chris Jungerius <cjungerius@gmail.com>
2020-12-08 12:03:02 +0100

    replaced metaparse with normal parsing of ints in day 8

--- 08/solution.jl
+++ 08/solution.jl
@@ -1,5 +1,3 @@
-instr = readlines("input.txt")
-
 function boot(instr::Array{String})
     repeat = Set()
     pos = 1
@@ -7,44 +5,53 @@ function boot(instr::Array{String})
     while true
         if pos > length(instr)
             return (0, acc)
-        elseif in(pos,repeat)
+        elseif in(pos, repeat)
             return (pos, acc)
         else 
-            push!(repeat,pos)
+            push!(repeat, pos)
         end
 
         if instr[pos][1:3] == "acc"
-            acc += Meta.parse(instr[pos][5:end])
+            acc += parse(Int, instr[pos][5:end])
             pos += 1
         elseif instr[pos][1:3] == "jmp"
-            pos += Meta.parse(instr[pos][5:end])
+            pos += parse(Int, instr[pos][5:end])
         else
             pos += 1
         end
     end
 end
 
-jumpornop = findall(instr) do line
-    occursin(r"(jmp|nop)", line)
-end
 
-for i in jumpornop
-    if instr[i][1:3] == "jmp"
-        instr[i] = replace(instr[i],"jmp" => "nop")
-        code, acc = boot(instr)
-        if code == 0
-            println(acc)
-            break
-        end
-        instr[i] = replace(instr[i],"nop" => "jmp")
-    else
-        instr[i] = replace(instr[i],"nop" => "jmp")
-        code, acc = boot(instr)
-        if code == 0
-            println(acc)
-            break
-        end
-        instr[i] = replace(instr[i],"jmp" => "nop")
+function fixedboot(instr)
+    jumpornop = findall(instr) do line
+        occursin(r"(jmp|nop)", line)
     end
 
+    for i in jumpornop
+        if instr[i][1:3] == "jmp"
+            instr[i] = replace(instr[i], "jmp" => "nop")
+            code, acc = boot(instr)
+            if code == 0
+                return acc
+                break
+            end
+            instr[i] = replace(instr[i], "nop" => "jmp")
+        else
+            instr[i] = replace(instr[i], "nop" => "jmp")
+            code, acc = boot(instr)
+            if code == 0
+                return acc
+                break
+            end
+            instr[i] = replace(instr[i], "jmp" => "nop")
+        end
+
+    end
+end
+
+function main()
+    instr = readlines("input.txt")
+    boot(instr)
+    fixedboot(instr)
 end
